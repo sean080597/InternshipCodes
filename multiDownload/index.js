ef54f08d60bd373cdbbadceac3facde2a529abbf
sean080597 <cuong.luusean@gmail.com>
2022-11-23 13:00:35 +0700

    Add new format json

--- multiDownload/index.js
+++ multiDownload/index.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 var request = require("request");
 
-const destPath = "downloaded/";
+const destPath = "downloaded";
 if (!fs.existsSync(destPath)) {
   fs.mkdirSync(destPath);
 }
@@ -15,6 +15,17 @@ var urlList = JSON.parse(data);
 //   "http://cdn.mos.musicradar.com/audio/samples/musicradar-wavedrum-samples.zip",
 // ];
 
+// var urlList = [
+//   {
+// 		"name": "ngay-trai-tim-roi-le-lofi",
+// 		"url": "https://dt.muvi.vn/mvn/track/song/2022/10/24/ngaytraitimroilelofimaster_20221024113722.mp3"
+// 	},
+// 	{
+// 		"name": "khong-tan-lofi-teed",
+// 		"url": "https://dt.muvi.vn/mvn/track/song/2022/10/17/0-tandickson-nguyen-lofinam-viet_20221017105135.mp3"
+// 	}
+// ];
+
 var download = function (url, dest, callback) {
   request
     .get(url)
@@ -25,10 +36,19 @@ var download = function (url, dest, callback) {
     .on("close", callback);
 };
 
-urlList.forEach(function (str) {
-  var filename = str.split("/").pop();
+urlList.forEach(function (el) {
+  let filename = "";
+  let url = "";
+  if (typeof el === "string") {
+    filename = el.split("/").pop();
+    url = el;
+  } else if (el.hasOwnProperty("name") && el.hasOwnProperty("url")) {
+    filename = el.name + "." + el.url.split(".").pop();
+    url = el.url;
+  }
+
   console.log("Downloading " + filename);
-  download(str, destPath + filename, function () {
+  download(url, destPath + "/" + filename, function () {
     console.log("Finished Downloading: " + filename);
   });
 });
