45f54e017d01ff26d126233afa7c2899cd2c8d10
sean080597 <cuong.luusean@gmail.com>
2023-04-27 15:06:35 +0700

    [microservices-spring] - Service Discovery & LoadBalanced WebClient

--- microservices-spring/inventory-service/src/main/java/com/programming/technie/inventoryservice/InventoryServiceApplication.java
+++ microservices-spring/inventory-service/src/main/java/com/programming/technie/inventoryservice/InventoryServiceApplication.java
@@ -5,8 +5,10 @@ import com.programming.technie.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class InventoryServiceApplication {
 
--- microservices-spring/order-service/src/main/java/com/programming/technie/orderservice/service/OrderService.java
+++ microservices-spring/order-service/src/main/java/com/programming/technie/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 public class OrderService {
 
   private final OrderRepository orderRepository;
-  private final WebClient webClient;
+  private final WebClient.Builder webClientBuilder;
 
   public void placeOrder(OrderRequest req) {
     Order order = new Order();
@@ -38,8 +38,8 @@ public class OrderService {
 
 
     // call Inventory Service and place order if product is in stock
-    InventoryResponse[] inventoryResponseArr = webClient.get()
-        .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+    InventoryResponse[] inventoryResponseArr = webClientBuilder.build().get()
+        .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
         .retrieve()
         .bodyToMono(InventoryResponse[].class)
         .block();
