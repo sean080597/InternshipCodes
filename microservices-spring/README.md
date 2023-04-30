2eead307ee4bc050925b154fe2bb036124c187fd
sean080597 <cuong.luusean@gmail.com>
2023-04-30 17:28:03 +0700

    [microservices-spring] - Add Distributed Tracing system Zipkin & Sleuth

--- microservices-spring/README.md
+++ microservices-spring/README.md
@@ -12,7 +12,8 @@ You can watch the tutorial on Youtube here - https://www.youtube.com/watch?v=mPP
 ## How to run the application without Docker
 
 1. Run `docker run -p 8181:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -v %cd%/data/keycloak:/opt/keycloak/data/h2 quay.io/keycloak/keycloak:21.1.1 start-dev` to start Keycloak.
-2. Run `docker run -it -p 27017:27017 -v %cd%/data/db:/data/db mongo:4.4` to start mongoDB Docker.
+2. Run `docker run -p 9411:9411 openzipkin/zipkin` to start Zipkin Docker.
+3. Run `docker run -it -p 27017:27017 -v %cd%/data/db:/data/db mongo:4.4` to start MongoDB Docker.
 3. Start MySQL DB: `shopii-order`, `shopii-inventory` by Xampp or something else.
 4. Run `mvn clean verify -DskipTests` by going inside each folder to build the applications.
 5. After that run `mvn spring-boot:run` by going inside each folder to start the applications.
--- microservices-spring/order-service/src/main/java/com/programming/technie/orderservice/service/OrderService.java
+++ microservices-spring/order-service/src/main/java/com/programming/technie/orderservice/service/OrderService.java
@@ -7,6 +7,9 @@ import com.programming.technie.orderservice.model.Order;
 import com.programming.technie.orderservice.model.OrderLineItem;
 import com.programming.technie.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -19,10 +22,12 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
   private final OrderRepository orderRepository;
   private final WebClient.Builder webClientBuilder;
+  private final Tracer tracer;
 
   public String placeOrder(OrderRequest req) {
     Order order = new Order();
@@ -36,19 +41,25 @@ public class OrderService {
 
     List<String> skuCodes = order.getOrderLineItemList().stream().map(OrderLineItem::getSkuCode).collect(Collectors.toList());
 
-    // call Inventory Service and place order if product is in stock
-    InventoryResponse[] inventoryResponseArr = webClientBuilder.build().get()
-        .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-        .retrieve()
-        .bodyToMono(InventoryResponse[].class)
-        .block();
+    log.info("Calling inventory service");
+    Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+    try (Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())) {
+      // call Inventory Service and place order if product is in stock
+      InventoryResponse[] inventoryResponseArr = webClientBuilder.build().get()
+          .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+          .retrieve()
+          .bodyToMono(InventoryResponse[].class)
+          .block();
 
-    boolean allProductsInStock = Arrays.stream(inventoryResponseArr).allMatch(InventoryResponse::isInStock);
-    if (allProductsInStock) {
-      orderRepository.save(order);
-      return "Order placed successfully";
-    } else {
-      throw new IllegalArgumentException("Product is not in stock, please try again later");
+      boolean allProductsInStock = Arrays.stream(inventoryResponseArr).allMatch(InventoryResponse::isInStock);
+      if (allProductsInStock) {
+        orderRepository.save(order);
+        return "Order placed successfully";
+      } else {
+        throw new IllegalArgumentException("Product is not in stock, please try again later");
+      }
+    } finally {
+      inventoryServiceLookup.end();
     }
   }
 
